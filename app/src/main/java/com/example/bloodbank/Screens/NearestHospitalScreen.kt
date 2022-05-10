e3ea87ac6d5b22900cfb9363ce981fb4142970f3
Tush1810 <tusharpruth@gmail.com>
2022-05-10 12:09:58 +0530

    .gitignore updated

--- app/src/main/java/com/example/bloodbank/Screens/NearestHospitalScreen.kt
+++ app/src/main/java/com/example/bloodbank/Screens/NearestHospitalScreen.kt
@@ -10,25 +10,26 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
+
 @Composable
 fun NearestHospitalScreen(navController: NavHostController){
-    var uiSettings by remember { mutableStateOf(MapUiSettings()) }
-    var properties by remember {
-        mutableStateOf(MapProperties(mapType = MapType.SATELLITE))
-    }
-    Box(Modifier.fillMaxSize()) {
-        GoogleMap(
-            modifier = Modifier.matchParentSize(),
-            properties = properties,
-            uiSettings = uiSettings
-        )
-        Switch(
-            checked = uiSettings.zoomControlsEnabled,
-            onCheckedChange = {
-                uiSettings = uiSettings.copy(zoomControlsEnabled = it)
-            }
-        )
-    }
+//    var uiSettings by remember { mutableStateOf(MapUiSettings()) }
+//    var properties by remember {
+//        mutableStateOf(MapProperties(mapType = MapType.SATELLITE))
+//    }
+//    Box(Modifier.fillMaxSize()) {
+//        GoogleMap(
+//            modifier = Modifier.matchParentSize(),
+//            properties = properties,
+//            uiSettings = uiSettings
+//        )
+//        Switch(
+//            checked = uiSettings.zoomControlsEnabled,
+//            onCheckedChange = {
+//                uiSettings = uiSettings.copy(zoomControlsEnabled = it)
+//            }
+//        )
+//    }
 }
 
 @Preview
